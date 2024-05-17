ac1e30f7eb3d4a72ee2426e0a05bdd409c917afe
John Chen <czhttp@gmail.com>
2024-05-17 12:54:13 +1200

    Update index.tsx

--- components/page-intro/index.tsx
+++ components/page-intro/index.tsx
@@ -55,7 +55,7 @@ const PageIntro = () => {
             <li>
               <i className="icon-shipping"></i>
               <div className="data-item__content">
-                <h4>99% Satisfied Customers</h4>
+                <h4>100% Satisfied Customers</h4>
                 <p>Our clients' opinions speak for themselves</p>
               </div>
             </li>
