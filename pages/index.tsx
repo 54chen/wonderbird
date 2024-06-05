b1777c9b74c303635f8182b48f79d591f8f031ab
wanghao <wanghao2017117@example.com>
2024-06-05 23:12:03 +1200

    123

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,7 @@ const IndexPage = ({featuredProducts}:{featuredProducts:ProductLists}) => {
           <article style={{backgroundImage: 'url(/images/featured-3.jpg)'}} className="featured-item featured-item-small">
             <div className="featured-item__content">
               <h3>Tattoo<br/>Furniture</h3>
-              <a href="/product/newatist" className="btn btn--rounded">VIEW ALL</a>
+              <a href="/product/tattoofurniture" className="btn btn--rounded">VIEW ALL</a>
             </div>
           </article>
         </div>
