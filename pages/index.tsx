8735d52880fea6fbb1dad731c705f47afa5fab18
54chen <czhttp@gmail.com>
2024-08-09 20:59:18 +1200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Layout from '../layouts/Main';
-import Bird from '../assets/icons/Bird';
+import Bird from '../assets/icons/Bird/index.js';
 import { useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
